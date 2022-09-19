9d8f695b54983081172aa8df2f332893c16c5877
Kulesko <thegreg@freenet.de>
2022-09-19 20:54:50 +0200

    refactor(colors): annotate Accept & Content-Type headers explicitly

--- src/main/java/com/kulesko/colors/rest/ColorResource.java
+++ src/main/java/com/kulesko/colors/rest/ColorResource.java
@@ -5,9 +5,13 @@ import com.kulesko.colors.json.SearchQuery;
 import com.kulesko.colors.service.ColorsService;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
+
 
 
 @Path("/v1/colors")
@@ -17,6 +21,8 @@ public class ColorResource {
     ColorsService colorsService;
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
     public List<ColorPuppy> matchColors(SearchQuery body) {
 
         return colorsService.getColor(body);
